e10e30dba3813c2487d6ad30e2957f8115413f5a
Dahbi <54712900+dleila@users.noreply.github.com>
2019-10-15 08:36:35 -0500

    Add files via upload
    Writing down the questions that I have to answer.

--- FunWithMath.py
+++ FunWithMath.py
@@ -19,9 +19,28 @@ def archimedes(numSides):
 print(archimedes(8))
 print(archimedes(16))
 
-for sides in range(2, 100, 2):
+for sides in range(8, 100, 8):
     print(sides, archimedes(sides))
 
 # Experiment with the loop above alongside the actual value of Pi.
 # How many sides does it take to make these two close?
     # It takes 2 sides to make the answer be the same on the second replay of the loop.
+
+# Accumulators
+
+acc = 0
+for x in range(1, 6):
+    acc = acc + x
+# We basically added 1+2+3+4+5 to get 15.
+
+print(acc)
+
+# Compute the sum of the first 100 even numbers.
+# Compute the sum of the first 50 odd numbers.
+# Compute the average of the first 100 odd numbers
+# Write a function that returns the average of the first N numbers, where N is a parameter.
+# Write a function called factorial that computes the product of the first N numbers, where N is a parameter.
+# Each number the tht Fibonacci sequence is the sum of the previous two numbers.
+    # First two numbers in sequence are 1 and 1. Compute the 10th Fibonacci number
+# Write a function to compute the Nth Fibonacci number where N is a parameter.
+    # You may assume that N will be grater than or equal to 3.
